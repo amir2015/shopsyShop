c05c3dc69ab46bbb8f3962ac93e35a91d55306a0
amir2015 <zahranovich2@gmail.com>
2022-01-17 10:25:51 +0200

    errors handling middleWare

--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,20 @@ import express from "express";
 import connectDB from "./config/db.js";
 import products from "./data/products.js";
 import productRoutes from "./routes/productRoutes.js";
-
+import {
+  notFound,
+  errorHandler,
+} from "../backend/middleWares/errorMiddleware.js";
 connectDB();
 
 const app = express();
-
+app.use("/api/products", productRoutes);
 app.get("/", (req, res) => {
   res.send("API is running....");
 });
-app.use("/api/products", productRoutes);
+app.use(notFound);
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
