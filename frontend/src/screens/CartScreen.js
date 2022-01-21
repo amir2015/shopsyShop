cf86546c258ad8acf1587444907e318f5329bfc3
amir2015 <zahranovich2@gmail.com>
2022-01-21 23:06:55 +0200

    cart-screen-done

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -17,7 +17,10 @@ import {
   Form,
   Button,
   Card,
+  ListGroupItem,
 } from "react-bootstrap";
+import Message from "../components/Message.js";
+
 const CartScreen = ({}) => {
   const { id } = useParams();
   const productId = id;
@@ -26,15 +29,100 @@ const CartScreen = ({}) => {
   const dispatch = useDispatch();
   const cart = useSelector((state) => state.cart);
   const { cartItems } = cart;
+  let navigate = useNavigate();
+
   console.log(cartItems);
   useEffect(() => {
     if (productId) {
       dispatch(addToCart(productId, qty));
     }
   }, [dispatch, productId, qty]);
+  const removeFromCartHandler = (id) => {
+    console.log("remove");
+  };
+  const checkOutHandler = () => {
+    navigate(`/login?redirect=shipping`);
+  };
   return (
     <>
-      <div>here is the cart</div>
+      <Row>
+        <Col md={6}>
+          <h1>Your Shoping Cart</h1>
+          {cartItems === 0 ? (
+            <Message>
+              Your Cart Is Empty <Link to="/">Go Back</Link>
+            </Message>
+          ) : (
+            <ListGroup variant="flush">
+              {cartItems.map((item) => (
+                <ListGroup.Item key={item.product}>
+                  <Row>
+                    <Col md={2}>
+                      <Image src={item.image} alt={item.name} fluid rounded />
+                    </Col>
+                    <Col md={2}>${item.price}</Col>
+
+                    <Col md={2}></Col>
+                    <Col>
+                      {" "}
+                      <Form.Control
+                        as="select"
+                        value={item.qty}
+                        onChange={(e) =>
+                          dispatch(
+                            addToCart(item.product, Number(e.target.value))
+                          )
+                        }
+                      >
+                        {[...Array(item.countInStock).keys()].map((x) => (
+                          <option key={x + 1} value={x + 1}>
+                            {x + 1}
+                          </option>
+                        ))}
+                      </Form.Control>
+                    </Col>
+                    <Col md={2}>
+                      <Button
+                        type="button"
+                        variant="light"
+                        onClick={() => removeFromCartHandler(item.product)}
+                      >
+                        <i className="fa fa-trash"></i>
+                      </Button>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+              ))}
+            </ListGroup>
+          )}
+        </Col>
+        <Col md={4}>
+          <Card>
+            <ListGroup.Item>
+              <h3>
+                Subtotal : ({cartItems.reduce((acc, item) => acc + item.qty, 0)}
+                )
+              </h3>
+              $
+              {cartItems
+                .reduce((acc, item) => acc + item.qty * item.price, 0)
+                .toFixed(2)}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Button
+                type="button"
+                className="btn-block"
+                disabled={cartItems.length === 0}
+                onClick={checkOutHandler}
+              >
+                {" "}
+                Proceed o Checkout
+              </Button>
+            </ListGroup.Item>
+          </Card>
+        </Col>
+        <Col md={2}></Col>
+      </Row>
     </>
   );
 };
