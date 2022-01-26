bffd696619b2ca678375733ac946b7b63f82ebb2
amir2015 <zahranovich2@gmail.com>
2022-01-26 14:22:38 +0200

    backend registeration works

---
+++ backend/controllers/userController.js
@@ -0,0 +1,68 @@
+import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
+import generateToken from "../utils/generateToken.js";
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user && (await user.matchPassword(password))) {
+    res.send({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
+  console.log(req.body.email);
+  res.send(req.body.email);
+});
+//get user profile
+//get   /api/users/profile
+//  private
+const getUSerProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById();
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User Not Found");
+  }
+});
+//register new user
+//get   /api/users/profile
+//  private
+const registertUSer = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User Already Exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user ");
+  }
+});
+export { authUser, registertUSer, getUSerProfile };
