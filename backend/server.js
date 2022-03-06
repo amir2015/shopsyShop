67960328a9403a64d9a1d4c08e282d9a1324aba4
amir2015 <zahranovich2@gmail.com>
2022-03-07 01:25:29 +0200

    deploying

--- backend/server.js
+++ backend/server.js
@@ -24,16 +24,25 @@ app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/upload", uploadRoutes);
-
-app.get("/", (req, res) => {
-  res.send("API is running....");
-});
-
 const __dirname = path.resolve();
+
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 app.get("/api/config/paypal", (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID);
 });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
+
 app.use(notFound);
 app.use(errorHandler);
 
