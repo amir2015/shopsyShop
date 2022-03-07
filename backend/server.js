8294f5e70e7fcfeeb6cc26720ab0c5ecb3af0c48
amir2015 <zahranovich2@gmail.com>
2022-03-07 15:27:28 +0200

    fixed2

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.get("/api/config/paypal", (req, res) => {
 });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
