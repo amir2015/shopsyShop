565c69c5f43859b5f8b30844c7b72906274b72a3
amir2015 <zahranovich2@gmail.com>
2022-03-09 12:02:08 +0200

    add

--- backend/server.js
+++ backend/server.js
@@ -24,25 +24,21 @@ app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/upload", uploadRoutes);
+const __dirname = path.resolve();
 
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 app.get("/api/config/paypal", (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID);
 });
 
 if (process.env.NODE_ENV === "production") {
-  const __dirname = path.resolve();
-  app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
   app.use(express.static(path.join(__dirname, "../frontend/build")));
-  app.get("*", function (req, res) {
-    const index = path.join(__dirname, "/../frontend/build", "index.html");
-    res.sendFile(index);
-  });
 
-  // app.get("*", (req, res) => {
-  //   res.sendFile(
-  //     path.resolve(__dirname, "../", "frontend", "build", "index.html")
-  //   );
-  // });
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    );
+  });
 } else {
   app.get("/", (req, res) => {
     res.send("API is running....");
